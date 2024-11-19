95cc911e07106a71672cc2f9c197e52dfa4e25fa
Max112.87 <max112.87>
2024-11-19 14:23:05 +0700

    20

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import Link from 'next/link';
-import Navbar from './components/Navbar';
+
 import BottomNavigation from './components/BottomNavigation';
-import Example from './components/SimpleCentered';
-import DarkNav from './components/DarkNav'
+
 export default function Home() {
 	return (
 		<div className=''>
-			<DarkNav />
-			
-			<Navbar />
-		
 			<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center '>
 				<div>
 					<Link href='/english'>
