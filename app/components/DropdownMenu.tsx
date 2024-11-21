79cc691000cb63470f3c21b50321359e82c9e7ec
Max112.87 <max112.87>
2024-11-21 11:18:43 +0700

    29

--- app/components/DropdownMenu.tsx
+++ app/components/DropdownMenu.tsx
@@ -26,26 +26,23 @@ export default function DropdownMenu({
 		<div className='relative inline-block text-left'>
 			<Menu>
 				<div>
-					<MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
+					<MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-6 py-3 text-xl font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
 						{selectedLabel}
 						<ChevronDownIcon
 							aria-hidden='true'
-							className='-mr-1 size-5 text-gray-400'
+							className='-mr-1 h-6 w-6 text-gray-400'
 						/>
 					</MenuButton>
 				</div>
 
-				<MenuItems
-					className='absolute mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'
-					
-				>
+				<MenuItems className='absolute mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none text-lg'>
 					<div className='py-1'>
 						{options.map((option, index) => (
 							<MenuItem key={index}>
 								{({ active }) => (
 									<button
 										onClick={() => handleSelect(option.label, option.onClick)}
-										className={`block w-full px-4 py-2 text-left text-sm ${
+										className={`block w-full px-6 py-3 text-left text-lg ${
 											active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
 										}`}
 									>
