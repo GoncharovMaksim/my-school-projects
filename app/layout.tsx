d6a0c248f2d343499c1a1c262366eaed85d09f16
Max112.87 <max112.87>
2024-11-24 13:28:16 +0700

    53

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,10 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 min-h-screen flex flex-col`}
 			>
 				<DarkNav />
-				{children}
+				<main className='flex-1 pb-[60px]'>
+					{/* отступ равен высоте меню */}
+					{children}
+				</main>
 				<BottomNavigation />
 			</body>
 		</html>
