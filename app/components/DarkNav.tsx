90a837d149f8b54b1f2b8cc290fbe55a450b925c
Max112.87 <max112.87>
2024-11-28 19:16:27 +0700

    86

--- app/components/DarkNav.tsx
+++ app/components/DarkNav.tsx
@@ -46,7 +46,8 @@ const user = {
 	name: session.data?.user?.name,
 	email: session.data?.user?.email,
 	imageUrl:
-		session.data?.user?.image,
+		session?.data?.user?.image ||
+		'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
 	return (
 		<>
