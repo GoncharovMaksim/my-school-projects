aa6a088da86c43a36b54de24f1d7b19d8dfdec6c
Max112.87 <max112.87>
2024-11-28 22:21:09 +0700

    93

--- app/components/TgApi.tsx
+++ app/components/TgApi.tsx
@@ -1,22 +1,20 @@
-export default function TgApi(message:string) {
-	const TOKEN = process.env.TOKEN; // Токен твоего бота
-	const CHAT_ID = process.env.CHAT_ID; // ID чата
-
-	fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			chat_id: CHAT_ID,
-			text: message,
-		}),
-	})
-		.then(response => response.json())
-		.then(data => {
-			console.log('Успешно отправлено:', data);
-		})
-		.catch(error => {
-			console.error('Ошибка при отправке:', error);
+export default async function TgApi(message: string) {
+	try {
+		const response = await fetch('/api/tgApi', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ message }),
 		});
+
+		if (!response.ok) {
+			throw new Error('Ошибка отправки сообщения');
+		}
+
+		const data = await response.json();
+		console.log('Сообщение успешно отправлено:', data);
+	} catch (error) {
+		console.error('Ошибка:', error);
+	}
 }
