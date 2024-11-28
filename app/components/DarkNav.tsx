e3e8308046e5201601e42d631ef3f05b6ca2cdd3
Max112.87 <max112.87>
2024-11-28 19:26:59 +0700

    87

--- app/components/DarkNav.tsx
+++ app/components/DarkNav.tsx
@@ -210,6 +210,13 @@ const user = {
 										as='a'
 										href={item.href}
 										className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+										onClick={e => {
+											// Если есть onClick, выполнить его и предотвратить переход по ссылке
+											if (item.onClick) {
+												e.preventDefault();
+												item.onClick();
+											}
+										}}
 									>
 										{item.name}
 									</DisclosureButton>
