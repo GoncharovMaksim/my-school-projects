c938e3917dc048ac9d58cafa9e59291cf7a662f3
Max112.87 <max112.87>
2024-11-28 19:49:25 +0700

    88

--- app/components/DarkNav.tsx
+++ app/components/DarkNav.tsx
@@ -12,7 +12,6 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useSession, signOut } from 'next-auth/react';
 import Link from 'next/link';
 
-
 const navigation = [
 	{ name: 'Главное меню', href: '/', current: true },
 	{ name: 'Математика', href: '/math', current: false },
@@ -30,25 +29,23 @@ export default function DarkNav() {
 	console.log(session);
 	const userNavigation = session.data
 		? [
-				{ name: 'Your Profile', href: '/profile' },
-				{ name: 'Settings', href: '#' },
+				{ name: 'Профиль', href: '/profile' },
 				{
-					name: 'Sign out',
+					name: 'Выход',
 					href: '#',
 					onClick: () => signOut({ callbackUrl: '/' }),
 				},
 		  ]
 		: [
-				{ name: 'Sign in', href: '/api/auth/signin' },
-				{ name: 'Register', href: '/register' },
+				{ name: 'Вход', href: '/api/auth/signin' },
+				{ name: 'Регистрация', href: '/register' },
 		  ];
-const user = {
-	name: session.data?.user?.name,
-	email: session.data?.user?.email,
-	imageUrl:
-		session?.data?.user?.image ||
-		'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
+	const user = {
+		name: session.data?.user?.name || 'Гость',
+		email: session.data?.user?.email || '',
+		imageUrl: session?.data?.user?.image || '/globe.svg',
+	};
+
 	return (
 		<>
 			{/*
