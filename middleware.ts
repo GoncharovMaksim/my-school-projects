58e43a3859947a96baf4b7aab7955eff40c3634a
Max112.87 <max112.87>
2024-11-29 15:44:56 +0700

    101

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-export {default} from 'next-auth/middleware'
+export { default } from 'next-auth/middleware';
 
-
-export const config = {matcher: ['/profile', '/math', '/english']}
+export const config = {
+	matcher: ['/profile', '/math', '/english', '/statistics'],
+};
