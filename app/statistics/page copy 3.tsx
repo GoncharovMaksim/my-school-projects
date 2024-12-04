0dace3aed7372c19e90ec658f59d35d31ff51255
Max112.87 <max112.87>
2024-12-04 19:41:00 +0700

    123

--- app/statistics/page copy 3.tsx
+++ app/statistics/page copy 3.tsx
@@ -59,7 +59,7 @@ export default async function App(props: { searchParams: SearchParams }) {
 	}
 
 	return (
-		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center '>
+		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center'>
 			<div className='p-8 flex flex-col items-center space-y-6'>
 				<h1 className='text-4xl text-center font-bold mb-4'>Статистика</h1>
 				<Filters />
