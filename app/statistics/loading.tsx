40de4f972b7b1b1a7f9a0d0b47e7eeadb50fbaea
Max112.87 <max112.87>
2024-12-04 13:29:55 +0700

    121

---
+++ app/statistics/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+	return (
+		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center'>
+			<div className='p-8 flex flex-col items-center space-y-6'>
+				<h1 className='text-4xl text-center font-bold mb-4'>Статистика</h1>
+				<h3 className='text-2xl text-center font-bold mb-4'>
+					Загрузка данных...
+				</h3>
+			</div>
+		</div>
+	);
+}
