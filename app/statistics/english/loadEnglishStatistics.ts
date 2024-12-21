71cd6d3218314ea7e11ab7f1b672e13373009b05
Max112.87 <max112.87>
2024-12-21 23:37:03 +0700

    194

--- app/statistics/english/loadEnglishStatistics.ts
+++ app/statistics/english/loadEnglishStatistics.ts
@@ -2,9 +2,9 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 
 export const loadEnglishStatistics = createAsyncThunk(
-	'statistics/loadEnglishStatistics', // Название для экшн-тега
+	'statistics/loadEnglishStatistics', 
 	async (_, { rejectWithValue }) => {
-		// Используем пустой аргумент, так как он не используется в вашем случае
+		
 		const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/gameStatistics/english`;
 
 		try {
@@ -15,10 +15,10 @@ export const loadEnglishStatistics = createAsyncThunk(
 			}
 
 			const data = await response.json();
-			return data; // Возвращаем данные для сохранения в хранилище
+			return data; 
 		} catch (error) {
 			console.error('Ошибка при получении данных:', error);
-			return rejectWithValue('Ошибка при загрузке статистики'); // Возвращаем ошибку через `rejectWithValue`
+			return rejectWithValue('Ошибка при загрузке статистики'); 
 		}
 	}
 );
