0646df751d3e3111aec31401733629b746d853ef
Max112.87 <max112.87>
2024-12-27 11:47:30 +0700

    208

--- app/english/page.tsx
+++ app/english/page.tsx
@@ -21,18 +21,23 @@ export default function App() {
 
 	return (
 		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center '>
-			<h1 className='text-4xl text-center font-bold mb-4'>Английский</h1>
+			<div className='my-8 flex flex-col items-center space-y-6'>
+				<h1 className='text-4xl text-center font-bold mb-4 '>Английский</h1>
 
-			{!gameSettings.gameStatus ? (
-				<Settings
-					gameSettings={gameSettings}
-					setGameSettings={setGameSettings}
-				/>
-			) : (
-				<>
-					<Game gameSettings={gameSettings} setGameSettings={setGameSettings} />
-				</>
-			)}
+				{!gameSettings.gameStatus ? (
+					<Settings
+						gameSettings={gameSettings}
+						setGameSettings={setGameSettings}
+					/>
+				) : (
+					<>
+						<Game
+							gameSettings={gameSettings}
+							setGameSettings={setGameSettings}
+						/>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
