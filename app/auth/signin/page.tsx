b39a30b529f834418586cc342ad03bd40a246ee6
Max112.87 <max112.87>
2025-01-13 17:37:19 +0700

    262

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,26 +1,28 @@
-'use client';
+// pages/signin.tsx (или ваш файл страницы)
 
 import { signIn } from 'next-auth/react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useState,  } from 'react';
+import { useState } from 'react';
+
+export async function getServerSideProps(context: { res: { setHeader: (arg0: string, arg1: string) => void; }; }) {
+	// Устанавливаем заголовки для отключения кеширования
+	context.res.setHeader('Cache-Control', 'no-store');
+
+	// Можно вернуть пустые props или что-то еще, если необходимо
+	return {
+		props: {}, // данные для страницы
+	};
+}
 
 export default function SignInPage() {
 	const router = useRouter();
 	const searchParams = useSearchParams();
-	const callbackUrl = searchParams.get('callbackUrl') || '/';
+	const callbackUrl = searchParams.get('callbackUrl') || '/'; // Получаем изначальную страницу или используем `/`
 
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [nickName, setNickName] = useState('');
 	const [error, setError] = useState('');
-
-	// Функция для проверки сессии
-	const checkSession = async () => {
-		const session = await fetch('/api/auth/session').then(res => res.json());
-		if (session?.user) {
-			router.push(callbackUrl);
-		}
-	};
 
 	const handleSignIn = async (e: React.FormEvent) => {
 		e.preventDefault();
@@ -40,8 +42,7 @@ export default function SignInPage() {
 				console.log('Callback URL:', callbackUrl);
 				console.log('SignIn Result:', result);
 
-				// Проверяем сессию и перенаправляем пользователя
-				checkSession();
+				router.push(callbackUrl); // Перенаправляем на изначальную страницу
 			}
 		} catch (err) {
 			setError('Ошибка при входе. Попробуйте снова.');
@@ -50,10 +51,9 @@ export default function SignInPage() {
 	};
 
 	return (
-		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center '>
+		<div className='container mx-auto px-4 flex flex-col space-y-6 max-w-screen-sm items-center'>
 			<div className='p-8 flex flex-col items-center space-y-6'>
 				<h1 className='text-4xl text-center font-bold mb-4'>Вход</h1>
-
 				<form onSubmit={handleSignIn} className='w-full max-w-md space-y-4'>
 					{error && <div className='text-red-500 text-sm'>{error}</div>}
 					<div className='flex flex-col'>
