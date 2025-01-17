deba612557c7b03f31ff29165dbc617763f38bf6
Max112.87 <max112.87>
2025-01-18 01:36:56 +0700

    320

--- app/layout copy 2.tsx
+++ app/layout copy 2.tsx
@@ -4,7 +4,6 @@ import './globals.css';
 import DarkNav from '../components/DarkNav';
 import Providers from '../components/Providers';
 import BottomNavigation from '@/components/BottomNavigation';
-import { Rubik_Doodle_Shadow } from 'next/font/google';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -18,10 +17,9 @@ const geistMono = localFont({
 });
 
 
-const rubikDoodleShadow = Rubik_Doodle_Shadow({
+const rubikDoodleShadow = localFont({
+	src: './fonts/RubikDoodleShadow-Regular.ttf',
 	variable: '--font-rubik-doodle-shadow',
-	subsets: ['cyrillic'],
-	display: 'swap',
 	weight: '400', // Укажите корректный вес шрифта, если он известен
 });
 
