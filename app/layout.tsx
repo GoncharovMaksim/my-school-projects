13100e8095ce0ef575175c72f24dbe078be6c4d9
Max112.87 <max112.87>
2025-01-17 22:46:52 +0700

    316

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import DarkNav from '../components/DarkNav';
 import Providers from '../components/Providers';
 import BottomNavigation from '@/components/BottomNavigation';
+import Head from 'next/head'; // Импортируем Head
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -16,14 +17,11 @@ const geistMono = localFont({
 	weight: '100 900',
 });
 
-
 const rubikDoodleShadow = localFont({
 	src: './fonts/RubikDoodleShadow-Regular.ttf',
 	variable: '--font-rubik-doodle-shadow',
-	weight: '400', // Укажите корректный вес шрифта, если он известен
+	weight: '400',
 });
-
-
 
 export const metadata: Metadata = {
 	title: 'Школа112',
@@ -37,6 +35,29 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
+			<Head>
+				<link
+					rel='preload'
+					href='/fonts/GeistVF.woff'
+					as='font'
+					type='font/woff'
+					crossOrigin='anonymous'
+				/>
+				<link
+					rel='preload'
+					href='/fonts/GeistMonoVF.woff'
+					as='font'
+					type='font/woff'
+					crossOrigin='anonymous'
+				/>
+				<link
+					rel='preload'
+					href='/fonts/RubikDoodleShadow-Regular.ttf'
+					as='font'
+					type='font/ttf'
+					crossOrigin='anonymous'
+				/>
+			</Head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} ${rubikDoodleShadow.variable} antialiased bg-gray-100 min-h-screen flex flex-col`}
 			>
