85303ac5241b23c22d0a3ebe12e4fa3d674b5798
Max112.87 <max112.87>
2025-08-17 14:31:52 +0700

    реализовано динамическое добавление классов английского из данных бд

--- app/english/components/Settings.tsx
+++ app/english/components/Settings.tsx
@@ -68,6 +68,7 @@ export default function Settings({ setGameSettings }: GameProps) {
         console.error('Ошибка загрузки слов:', err);
         dispatch(setError(true)); // Устанавливаем ошибку в состояние
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, wordsList.length]);
 
   useEffect(() => {
